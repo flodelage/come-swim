ace8641bbe0badcbffd30c616ddfd7afd83415ca
Florian Delage <delageflo@ymail.com>
2019-06-26 16:27:00 +0200

    add validations and associations to models

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,6 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :swimming_pool
+
+  validates :date, presence: true
 end
--- app/models/swimming_pool.rb
+++ app/models/swimming_pool.rb
@@ -1,2 +1,8 @@
 class SwimmingPool < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+  has_many :users, through: :bookings
+
+  validates :name, :address, :capacity, :price, presence: true
+  validates :price, numericality: { only_integer: true }
+  validates :availability, inclusion: { in: [true, false] }
 end
